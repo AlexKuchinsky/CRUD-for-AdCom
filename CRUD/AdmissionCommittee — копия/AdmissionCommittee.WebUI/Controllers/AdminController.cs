0d0218e6e99e86c9995f8dc70957857a6f79cc6f
Filfur <glotovartemalex@gmail.com>
2017-12-26 02:25:40 +0300

    Update 1 (almost final)
    Added next view: show, edit. Function of deleting and editing. Additional task was made too.

---
+++ CRUD/AdmissionCommittee — копия/AdmissionCommittee.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,94 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using AdmissionCommittee.Domain.Abstract;
+using AdmissionCommittee.Domain.Entities;
+using AdmissionCommittee.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IEnrolleeRepository repository;
+        public int PageSize = 5;
+
+        public AdminController(IEnrolleeRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(EducationLevel? edLevel = null, int page = 1)
+        {
+            EnrolleesListViewModel model = new EnrolleesListViewModel
+            {
+                Enrollees = repository.Enrollees
+                    .Where(p => edLevel == null || p.EducationLevel == edLevel)
+                    .OrderBy(p => p.EnrolleeID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = edLevel == null ?
+                        repository.Enrollees.Count() :
+                        repository.Enrollees.Where(e => e.EducationLevel == edLevel).Count()
+                },
+                CurrentCategoryOfEducation = edLevel
+            };
+            return View(model);
+        }
+
+        public ViewResult Edit(int enrolleeId)
+        {
+            Enrollee enrollee = repository.Enrollees
+                .FirstOrDefault(p => p.EnrolleeID == enrolleeId);
+            return View(enrollee);
+        }
+
+        public ViewResult Show(int enrolleeId)
+        {
+            Enrollee enrollee = repository.Enrollees
+                    .FirstOrDefault(p => p.EnrolleeID == enrolleeId);
+            return View(enrollee);
+        }
+
+        [HttpPost]
+        public ActionResult ChangeEducationLevel(EducationLevel edLevel)
+        {
+            return RedirectToAction("Index", new {edLevel });
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Enrollee enrollee)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveEnrollee(enrollee);
+                TempData["message"] = string.Format("{0} has been saved", enrollee.LastName + " " + enrollee.LastName);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // there is something wrong with the data values
+                return View(enrollee);
+            }
+        }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Enrollee());
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int enrolleeId)
+        {
+            Enrollee deletedEnrollee = repository.DeleteEnrollee(enrolleeId);
+            if (deletedEnrollee != null)
+            {
+                TempData["message"] = string.Format("{0} was deleted",
+                deletedEnrollee.LastName + " " + deletedEnrollee.FirstName);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CRUD/AdmissionCommittee — копия/AdmissionCommittee.WebUI/Models/EnrolleesListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using AdmissionCommittee.Domain.Entities;
+
+namespace AdmissionCommittee.WebUI.Models
+{
+    public class EnrolleesListViewModel
+    {
+        public IEnumerable<Enrollee> Enrollees { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public EducationLevel? CurrentCategoryOfEducation { get; set; }
+    }
+}
